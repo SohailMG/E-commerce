0375e85548d9a983b3c879951c84093c9cf16799
Sohail Gsais <gsais.sohail9@gmail.com>
2021-01-17 18:14:45 +0000

    added product grids

--- commonPHP/headerNav.php
+++ commonPHP/headerNav.php
@@ -6,8 +6,9 @@ function outputHTMLtags(){
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="styles/styles.css">
-        <title>Index</title>
+        <link rel="stylesheet" href="styles/common.css">
+        <link rel="stylesheet" href="styles/perfumes.css">
+        <title>PerfumeFest</title>
     </head>  
     <body>';
 
--- perfumes.php
+++ perfumes.php
@@ -1,9 +1,110 @@
 <?php
-    include_once('commonPHP/headerNav.php');
-    outputHTMLtags();
-    outputHeaderNav("Perfumes");
+include_once('commonPHP/headerNav.php');
+outputHTMLtags();
+outputHeaderNav("Perfumes");
 ?>
 
+<main>
+    <div class="gridwrapper">
+        <div class="item-one">
+            <div id="item-picture"></div>
+            <div id="item-name">Perfume Name</div>
+            <div id="item-price">£12.99</div>
+            <button id="addbtn">Add to Cart</button>
+        </div>
+        <div class="item-two">
+            <div id="item-picture"></div>
+            <div id="item-name">Perfume Name</div>
+            <div id="item-price">£12.99</div>
+            <button id="addbtn">Add to Cart</button>
+        </div>
+        <div class="item-one">
+            <div id="item-picture"></div>
+            <div id="item-name">Perfume Name</div>
+            <div id="item-price">£12.99</div>
+            <button id="addbtn">Add to Cart</button>
+        </div>
+        <div class="item-two">
+            <div id="item-picture"></div>
+            <div id="item-name">Perfume Name</div>
+            <div id="item-price">£12.99</div>
+            <button id="addbtn">Add to Cart</button>
+        </div>
+        <div class="item-one">
+            <div id="item-picture"></div>
+            <div id="item-name">Perfume Name</div>
+            <div id="item-price">£12.99</div>
+            <button id="addbtn">Add to Cart</button>
+        </div>
+        <div class="item-two">
+            <div id="item-picture"></div>
+            <div id="item-name">Perfume Name</div>
+            <div id="item-price">£12.99</div>
+            <button id="addbtn">Add to Cart</button>
+        </div>
+        <div class="item-one">
+            <div id="item-picture"></div>
+            <div id="item-name">Perfume Name</div>
+            <div id="item-price">£12.99</div>
+            <button id="addbtn">Add to Cart</button>
+        </div>
+        <div class="item-two">
+            <div id="item-picture"></div>
+            <div id="item-name">Perfume Name</div>
+            <div id="item-price">£12.99</div>
+            <button id="addbtn">Add to Cart</button>
+        </div>
+        <div class="item-one">
+            <div id="item-picture"></div>
+            <div id="item-name">Perfume Name</div>
+            <div id="item-price">£12.99</div>
+            <button id="addbtn">Add to Cart</button>
+        </div>
+        <div class="item-two">
+            <div id="item-picture"></div>
+            <div id="item-name">Perfume Name</div>
+            <div id="item-price">£12.99</div>
+            <button id="addbtn">Add to Cart</button>
+        </div>
+        <div class="item-one">
+            <div id="item-picture"></div>
+            <div id="item-name">Perfume Name</div>
+            <div id="item-price">£12.99</div>
+            <button id="addbtn">Add to Cart</button>
+        </div>
+        <div class="item-two">
+            <div id="item-picture"></div>
+            <div id="item-name">Perfume Name</div>
+            <div id="item-price">£12.99</div>
+            <button id="addbtn">Add to Cart</button>
+        </div>
+        <div class="item-one">
+            <div id="item-picture"></div>
+            <div id="item-name">Perfume Name</div>
+            <div id="item-price">£12.99</div>
+            <button id="addbtn">Add to Cart</button>
+        </div>
+        <div class="item-two">
+            <div id="item-picture"></div>
+            <div id="item-name">Perfume Name</div>
+            <div id="item-price">£12.99</div>
+            <button id="addbtn">Add to Cart</button>
+        </div>
+        <div class="item-one">
+            <div id="item-picture"></div>
+            <div id="item-name">Perfume Name</div>
+            <div id="item-price">£12.99</div>
+            <button id="addbtn">Add to Cart</button>
+        </div>
+        <div class="item-two">
+            <div id="item-picture"></div>
+            <div id="item-name">Perfume Name</div>
+            <div id="item-price">£12.99</div>
+            <button id="addbtn">Add to Cart</button>
+        </div>
+    </div>
+
+</main>
 
 
 
